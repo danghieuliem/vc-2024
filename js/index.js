2476efa861ddb1b83a480adf89421752e170ff32
Liêm Đặng <hieuliem33@gmail.com>
2024-05-23 23:51:22 +0700

    feat/add-animejs-make-gb-animetion

--- js/index.js
+++ js/index.js
@@ -5,3 +5,75 @@ document.getElementById("btn-download-cv").addEventListener(
   },
   false
 );
+
+const mainColorDarker = "#2C3333";
+const mainColorDark = "#2E4F4F";
+const mainColor = "#0E8388";
+const mainColorLight = "#CBE4DE";
+
+const itemSize = 100;
+const cowsNumber = parseInt(window.innerWidth / itemSize);
+const colsNumber = parseInt(window.innerHeight / itemSize);
+
+const initBackground = () => {
+  const arrayItem = [];
+  for (i = 0; i < cowsNumber * colsNumber; ++i) {
+    const item = document.createElement("span");
+    item.className = "el";
+    item.style.height = `${itemSize}px`;
+    item.style.width = `${itemSize}px`;
+    arrayItem.push(item);
+  }
+  document.querySelector(".background-anime").append(...arrayItem);
+};
+initBackground();
+
+// ** Animation
+
+anime({
+  targets: ".background-anime .el",
+  backgroundColor: [
+    {
+      value: mainColorDarker,
+      easing: "easeOutSine",
+      duration: 500,
+    },
+    {
+      value: mainColorDark,
+      easing: "easeInOutQuad",
+      duration: 500,
+    },
+    {
+      value: mainColor,
+      easing: "easeInOutQuad",
+      duration: 500,
+    },
+    {
+      value: mainColorLight,
+      easing: "easeInOutQuad",
+      duration: 500,
+    },
+    {
+      value: mainColor,
+      easing: "easeInOutQuad",
+      duration: 500,
+    },
+    {
+      value: mainColorDark,
+      easing: "easeInOutQuad",
+      duration: 500,
+    },
+    {
+      value: mainColorDarker,
+      easing: "easeOutSine",
+      duration: 500,
+    },
+  ],
+  delay: anime.stagger(200, {
+    grid: [cowsNumber, colsNumber],
+    from: "first",
+  }),
+  loop: true,
+  easing: "easeInOutExpo",
+  direction: "revert",
+});
